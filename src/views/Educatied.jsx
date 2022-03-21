8951230c818a7d5df89bad119c7c95f1799e1e00
Ekrem Sönmezer <ekremsonmezer@gmail.com>
2022-03-21 14:25:27 +0300

    views  added

---
+++ src/views/Educatied.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Educatied extends Component {
+    render() {
+        return (
+            <div>
+                <p>Educatied</p>
+            </div>
+        );
+    }
+}
+
+export default Educatied;
