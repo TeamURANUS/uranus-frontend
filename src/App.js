1daf8394d7452bbff6d9c781c2b92e3dea39a784
arslanmustafabora <mboraarslan1999@gmail.com>
2022-03-25 23:11:15 +0300

    add read write comment components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import ReadComment from "./components/comment/ReadComment";
+import WriteComment from "./components/comment/WriteComment";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <ReadComment name="Student-1"/>
+            <WriteComment name="Student-2"/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/comment/WriteComment.jsx
@@ -0,0 +1,94 @@
+import {Comment, Avatar, Form, Button, List, Input} from 'antd';
+import moment from 'moment';
+import React from "react";
+
+const {TextArea} = Input;
+
+
+const CommentList = ({comments}) => (
+    <List
+        dataSource={comments}
+        itemLayout="horizontal"
+        renderItem={props => <Comment {...props} />}
+    />
+);
+
+const Editor = ({onChange, onSubmit, submitting, value}) => (
+    <>
+        <Form.Item>
+            <TextArea rows={4} onChange={onChange} value={value}/>
+        </Form.Item>
+        <Form.Item>
+            <Button htmlType="submit" loading={submitting} onClick={onSubmit} type="primary">
+                Add Comment
+            </Button>
+        </Form.Item>
+    </>
+);
+
+class WriteComment extends React.Component {
+    state = {
+        comments: [],
+        submitting: false,
+        value: '',
+        name: 'Ekrem Sonmezer',
+        avatarID: Math.floor(Math.random() * 18)
+    };
+
+    handleSubmit = () => {
+        if (!this.state.value) {
+            return;
+        }
+
+        this.setState({
+            submitting: true,
+            name: this.props.name
+        });
+
+        setTimeout(() => {
+            this.setState({
+                submitting: false,
+                value: '',
+                comments: [
+                    ...this.state.comments,
+                    {
+                        author: this.state.name,
+                        avatar: 'https://joeschmoe.io/api/v1/' + this.state.avatarID.toString(),
+                        content: <p>{this.state.value}</p>,
+                        datetime: moment().fromNow(),
+                    },
+                ],
+            });
+        }, 1000);
+    };
+
+    handleChange = e => {
+        this.setState({
+            value: e.target.value,
+        });
+    };
+
+    render() {
+        const {comments, submitting, value} = this.state;
+
+        return (
+            <>
+                {comments.length > 0 && <CommentList comments={comments}/>}
+                <Comment
+                    avatar={<Avatar src={"https://joeschmoe.io/api/v1/" + this.state.avatarID.toString()}
+                                    alt="Han Solo"/>}
+                    content={
+                        <Editor
+                            onChange={this.handleChange}
+                            onSubmit={this.handleSubmit}
+                            submitting={submitting}
+                            value={value}
+                        />
+                    }
+                />
+            </>
+        );
+    }
+}
+
+export default WriteComment;
