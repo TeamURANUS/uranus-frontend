ce1b7e0bac58fb432661525087a5a0eb475514ef
Ekrem Sönmezer <ekremsonmezer@gmail.com>
2022-04-16 14:12:34 +0300

    post and comment read

--- src/components/educatied/home/ReadPost.jsx
+++ src/components/educatied/home/ReadPost.jsx
@@ -1,31 +1,56 @@
-import {Comment, Avatar, Tooltip, Card} from 'antd';
+import {Comment, Avatar, Tooltip} from 'antd';
 import moment from "moment";
-import 'antd/dist/antd.css';
-import './ReadPost.css'
-
-const randomAvatar = "https://joeschmoe.io/api/v1/" + Math.floor(Math.random() * 18).toString()
+import ReadComment from "./ReadComment";
+import React, {useEffect, useState} from "react";
+import {getUserById} from "../../../services/user";
+import WriteComment from "./WriteComment";
 
 const ReadPost = (props) => {
+
+    const[user,setUser] = useState({
+        id: "id",
+        name: "name"
+    });
+
+    useEffect(()=>{
+        setUser(props.users.filter(x=>x.id===props.post.author)[0])
+    }, [props.users, props.post.author])
+
+
     return (
-        <Card id="post-card">
-            <p id="title-text">{props.title}</p>
-            <Comment id="read-post"
-                     title={props.title}
-                     author={<a>{props.author}</a>}
-                     avatar={<Avatar src={randomAvatar} alt={props.name}/>}
-                     content={
-                         <p>
-                             {props.text}
-                         </p>
-                     }
-                     datetime={
-                         <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
-                             <span>{moment().fromNow()}</span>
-                         </Tooltip>
-                     }
+        <div className="mt-10">
+            <button className="px-8 mb-3 py-2 border border-sky-500 rounded hover:text-white hover:bg-sky-500"
+                    onClick={() => {
+                        props.setReadPost(false, 0);
+                    }}
+            >Go Back
+            </button>
+            <p className="text-xl font-bold max-w-[1400px]">{props.post.title}</p>
+            <Comment
+                className="text-xl"
+                author={<a className="text-xs font-bold">{user.name}</a>}
+                avatar={<Avatar src={"https://joeschmoe.io/api/v1/16" + user.id}/>}
+                content={
+                    <p className="max-w-[1400px] text-lg">
+                        {props.post.text}
+                    </p>
+                }
+                datetime={
+                    <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
+                        <span>{props.post.date}</span>
+                    </Tooltip>
+                }
             />
-        </Card>
+            <hr/>
+            <ul>
+                {props.post.comments.map((item, index) => {
+                    return <ReadComment index={index} key={index} id={item} users={props.users}
+                                     />;
+                })}
+            </ul>
+            <WriteComment/>
+        </div>
     );
-}
+};
 
 export default ReadPost;
