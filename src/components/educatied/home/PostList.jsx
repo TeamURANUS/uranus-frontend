7a9d044ca6f636e76374c4b344d4734466a49ae5
Ekrem Sönmezer <ekremsonmezer@gmail.com>
2022-04-16 14:13:21 +0300

    group content classes posts comments

--- src/components/educatied/home/PostList.jsx
+++ src/components/educatied/home/PostList.jsx
@@ -3,7 +3,10 @@ import React, {Component} from 'react';
 class PostList extends Component {
     render() {
         return (
-            <li className="border-b border-zinc-300 mb-3 max-w-[1400px] select-none">
+            <li onClick={() => {
+                this.props.setReadPost(true,this.props.index)
+            }}
+                className="border-b border-zinc-300 mb-3 max-w-[1400px] select-none">
                 <span className="text-zinc-400 block">{this.props.data.date}</span>
                 <span className="font-bold block my-1">{this.props.data.title.substring(0,180)}</span>
                 <p>{this.props.data.text.substring(0,285)}</p>
@@ -13,7 +16,7 @@ class PostList extends Component {
                         src={this.props.data.avatar}
                         alt="avatar"
                     />
-                    <span className="ml-2 text-xs font-bold text-zinc-500">{this.props.data.author}</span>
+                    <span className="ml-2 text-xs font-bold text-zinc-500"><span>{this.props.data.isAdmin ? "(Admin) ": ""}</span>{this.props.user.name}</span>
                 </p>
             </li>
         );
