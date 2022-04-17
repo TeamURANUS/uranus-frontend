67e0b463651cffe51117445cf981cee353d54086
Ekrem Sönmezer <ekremsonmezer@gmail.com>
2022-04-17 13:46:01 +0300

    content templates added

---
+++ src/components/educatied/settings/SettingsContent.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class SettingsContent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    async componentDidMount() {
+
+    }
+
+
+    render() {
+        return (
+            <div className="my-2 mx-2">
+                <p className="font-bold text-xl my-2">Settings</p>
+
+            </div>
+        );
+    }
+}
+
+export default SettingsContent;
