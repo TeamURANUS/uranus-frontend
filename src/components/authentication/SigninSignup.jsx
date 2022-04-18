2efbfad0239f02664229b0b3d336ed1798cd0e38
Ekrem Sönmezer <ekremsonmezer@gmail.com>
2022-04-18 03:03:15 +0300

    forgot password

--- src/components/authentication/SigninSignup.jsx
+++ src/components/authentication/SigninSignup.jsx
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import {Input, Button} from 'antd';
+import {Input, Button, Modal} from 'antd';
 import {UserOutlined, EyeInvisibleOutlined, EyeTwoTone, LockOutlined} from '@ant-design/icons';
 
-import {login, register} from "../../services/authentication";
+import {forgotPassword, login, register} from "../../services/authentication";
 import {getUserByEmail} from "../../services/user";
 
 const svgPath = process.env.PUBLIC_URL + '/svg/';
@@ -17,7 +17,8 @@ class SigninSignup extends Component {
             loginError: "",
             registerEmail: "",
             registerPassword: "",
-            registerError: ""
+            registerError: "",
+            forgotModal: false
         };
     }
 
@@ -71,9 +72,25 @@ class SigninSignup extends Component {
         }
     };
 
+    onForgotPassword = async () => {
+        this.setState({forgotModal: true});
+        await forgotPassword(this.state.loginEmail);
+    };
+
     render() {
         return (
+
             <div className="flex flex-row mx-auto place-content-center flex-wrap items-baseline h-screen">
+                <Modal
+                    closable={false}
+                    title="Password Reset"
+                    cancelButtonProps={{style: {display: 'none'}}}
+                    onOk={() => {
+                        this.setState({forgotModal: false});
+                    }}
+                    visible={this.state.forgotModal}>
+                    <p>Password reset link will send your email if it is registered </p>
+                </Modal>
                 <div className="max-w-lg min-w-[25%] mx-4  p-6 drop-shadow-xl bg-white rounded-md">
                     <img
                         className="w-80 m-auto"
@@ -96,6 +113,10 @@ class SigninSignup extends Component {
                         }}
                     />
                     <br/><br/>
+                    <button onClick={() => {
+                        this.onForgotPassword();
+                    }} className="block w-full text-right underline hover:text-red-500 cursor-pointer">Forgot Password?
+                    </button>
                     <p className="text-red-600">{this.state.loginError}</p>
                     <Button type="primary" size="large" onClick={() => {
                         this.login(this.state.loginEmail, this.state.loginPassword);
@@ -124,6 +145,7 @@ class SigninSignup extends Component {
                         }}
                     />
                     <br/><br/>
+                    <br/>
                     <p className="text-red-600">{this.state.registerError}</p>
                     <Button type="primary" size="large" onClick={() => {
                         this.register(this.state.registerEmail, this.state.registerPassword);
